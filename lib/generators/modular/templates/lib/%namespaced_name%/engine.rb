8a569f045c3581e857ff6a62c1c85df5c34c6a95
Thibault <thibault@appyhotel.com>
2015-02-06 16:23:46 +0700

    Add default configuration to engine.rb

--- lib/generators/modular/templates/lib/%namespaced_name%/engine.rb
+++ lib/generators/modular/templates/lib/%namespaced_name%/engine.rb
@@ -1,6 +1,25 @@
 <%= wrap_in_modules <<-rb.strip_heredoc
   class Engine < ::Rails::Engine
   #{'  isolate_namespace ' + camelized_modules}
+  #{'  paths["app/views"] << "app/views/' + namespaced_name + '"' }
+        paths["app/views"] << "app/views/samurai/contact"
+
+  initializer :append_migrations do |app|
+    unless app.root.to_s.match(root.to_s)
+      config.paths["db/migrate"].expanded.each do |p|
+        app.config.paths["db/migrate"] << p
+      end
+    end
+  end
+
+  config.to_prepare do
+    Dir.glob(Engine.root.join("app", "decorators", "**", "*_decorator*.rb")) do |c|
+      Rails.configuration.cache_classes ? require(c) : load(c)
+    end
+  end
+
   end
 rb
 %>
+
+
